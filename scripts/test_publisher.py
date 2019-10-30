51951084fb6aebe4532bd6ee9c8bcac1e7918a7b
AlejandroCastillejo <ale.acc@hotmail.com>
2019-10-30 19:39:23 +0100

    bug in map_visualization fixed

--- scripts/test_publisher.py
+++ scripts/test_publisher.py
@@ -4,7 +4,7 @@ import rospy
 from sensor_msgs.msg import NavSatFix
 
 def test_publisher():
-    uav_gps_pos_pub_1 = rospy.Publisher('uav1/dji_sdk/gps_position', NavSatFix, queue_size=10)
+    uav_gps_pos_pub_1 = rospy.Publisher('uav_1/dji_sdk/gps_position', NavSatFix, queue_size=10)
     uav_gps_pos_pub_2 = rospy.Publisher('uav2/dji_sdk/gps_position', NavSatFix, queue_size=10)
     rospy.init_node('test_publisher')
 
@@ -13,8 +13,11 @@ def test_publisher():
         gps_position_2 = NavSatFix()
         # gps_position_1.latitude = 37.091136
         # gps_position_1.longitude =  -5.872402
-        gps_position_1.latitude = 37.091136
-        gps_position_1.longitude =  -5.872402
+        # gps_position_1.latitude = 37.091136
+        # gps_position_1.longitude =  -5.872402
+        gps_position_1.latitude = 37.3565
+        gps_position_1.longitude =  -6.1264
+
         uav_gps_pos_pub_1.publish(gps_position_1)
         gps_position_2.latitude = 37.091241
         gps_position_2.longitude =  -5.872474
